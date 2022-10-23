8748b208f7c97ceb9ef44c3af9853c59623ed0db
doriischeng <dorisc712@gmail.com>
2022-10-23 16:23:53 +0800

    Added a container to set max width for the app

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Dictionary defaultKeyword="hello" />
-      </header>
-      <footer className="App-footer">
-        This project was coded by{" "}
-        <a href="https://www.itsmedoris.com">
-          Doris Cheng
-        </a>{" "}
-        and is{" "}
-        <a href="https://github.com/doriischeng">
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a href="https://chic-froyo-2b35bc.netlify.app">
-          hosted on Netlify.
-        </a>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <Dictionary defaultKeyword="hello" />
+        </header>
+        <footer className="App-footer">
+          This project was coded by{" "}
+          <a href="https://www.itsmedoris.com">
+            Doris Cheng
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/doriischeng">
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a href="https://chic-froyo-2b35bc.netlify.app">
+            hosted on Netlify.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
